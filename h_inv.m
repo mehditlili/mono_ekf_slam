5bc1ec17c3f60a6549002ac83993f323aea5aaf4
Mehdi <mehdi.tlili@gmail.com>
2015-10-23 20:37:10 +0200

    Correct equation for camera reprojection

--- h_inv.m
+++ h_inv.m
@@ -1,11 +1,11 @@
-function out = h_inv(z_r, z_theta, l_z, r_x, r_y, theta, focal_length)
+function out = h_inv(z_r, z_phi, l_z, r_x, r_y, theta, focal_length)
 %H_INV reprojects observed landmark from image plan to world
 % usins a assumption for landmark's depth
-% z_r and z_theta are the pixel observations in polar coordinates
+% z_r and z_phi are the pixel observations in polar coordinates
 %   Detailed explanation goes here
 
-x_cam = -z_r*cos(z_theta)*l_z/focal_length;
-y_cam = -z_r*sin(z_theta)*l_z/focal_length;
+x_cam = z_r*sin(z_phi)*l_z/focal_length;
+y_cam = z_r*cos(z_phi)*l_z/focal_length;
 
 rot_mat = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
 
@@ -13,3 +13,11 @@ out = rot_mat*[x_cam;y_cam; l_z] + [r_x; r_y; 0];
 
 end
 
+
+% syms z_r z_phi l_z r_x r_y theta focal_length
+% x_cam = z_r*sin(z_phi)*l_z/focal_length;
+% y_cam = z_r*cos(z_phi)*l_z/focal_length;
+% rot_mat = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
+% jacobian(rot_mat*[x_cam;y_cam; l_z] + [r_x; r_y; 0], [r_x, r_y, theta])
+% jacobian(rot_mat*[x_cam;y_cam; l_z] + [r_x; r_y; 0], [z_r, z_phi])
+% jacobian(rot_mat*[x_cam;y_cam; l_z] + [r_x; r_y; 0], [l_z])
