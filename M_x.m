299115c31553eb522369a37af0155fd0a54770e0
Mehdi <mehdi.tlili@gmail.com>
2015-10-23 20:36:36 +0200

    Make alpha noise smaller

--- M_x.m
+++ M_x.m
@@ -1,9 +1,9 @@
 function out = M_x(v, omega)
 %M Covariance matrix of noise in control space
-alpha1 = 0.1;
-alpha2 = 0.1;
-alpha3 = 0.1;
-alpha4 = 0.1;
+alpha1 = 0.001;
+alpha2 = 0.001;
+alpha3 = 0.001;
+alpha4 = 0.001;
 out = zeros(2, 2);
 out(1, 1) = alpha1*v*v + alpha2*omega*omega;
 out(2, 2) = alpha3*v*v + alpha4*omega*omega;
